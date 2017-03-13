744489de5dd89451228abb8384475b70e811663a
Justin Henry <justin@homeceuconnection.com>
2017-03-13 13:07:25 -0500

    Adjusted plugin behavior.

--- brightcove/disable-pause/plugin.js
+++ brightcove/disable-pause/plugin.js
@@ -1,8 +1,3 @@
 videojs.plugin('disablePause', function () {
     console.log("Pausing is disabled for this video");
-    var css = document.createElement('link');
-    css.setAttribute('type', 'text/css');
-    css.setAttribute('rel', 'stylesheet');
-    css.setAttribute('href', 'https://rawgit.com/HomeCEU/cdn/feature/brightcove-plugin/brightcove/disable-pause/plugin.css');
-    document.body.appendChild(css);
 });
