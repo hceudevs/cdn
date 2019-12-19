c73c5a81cc7b72a00717cb1a3c2d00df69d25185
jhenry123 <jhenry@cytracom.com>
2019-12-19 15:04:17 -0600

    debug output

--- brightcove/progress/src/client.ts
+++ brightcove/progress/src/client.ts
@@ -3,23 +3,25 @@ import {fromEvent, interval} from "rxjs";
 import {first, map}          from "rxjs/operators";
 
 export class ProgressPluginClient {
-    static readonly PING          = 'video.progress.ping';
-    static readonly GET_PROGRESS  = 'video.progress.get';
-    static readonly SEND_PROGRESS = 'video.progress.set';
-    hasPinged                     = false;
+    static readonly PING         = 'video.progress.ping';
+    static readonly GET_PROGRESS = 'video.progress.get';
+    static readonly SET_PROGRESS = 'video.progress.set';
+    hasPinged                    = false;
 
     constructor(private iframe: HTMLIFrameElement, private http: Http) {
         fromEvent(iframe.contentWindow, 'message')
             .pipe(map((event: any) => event.data))
             .subscribe(async (data) => {
-                console.log(data);
+                if (data.event) {
+                    console.log('CLIENT', data);
+                }
                 if (data.event === ProgressPluginClient.PING) {
                     if (!this.hasPinged) {
                         this.hasPinged = true;
                         await this.onReady();
                     }
                 }
-                if (data.event === ProgressPluginClient.SEND_PROGRESS) {
+                if (data.event === ProgressPluginClient.SET_PROGRESS) {
                     await this.http.setProgress(data);
                 }
             });
--- brightcove/progress/src/plugin.ts
+++ brightcove/progress/src/plugin.ts
@@ -5,16 +5,20 @@ import {async}                                 from "rxjs/internal/scheduler/asy
 declare const videojs: any;
 
 export class ProgressPlugin {
-    static readonly PING          = 'video.progress.ping';
-    static readonly PONG          = 'video.progress.pong';
-    static readonly GET_PROGRESS  = 'video.progress.get';
-    static readonly SEND_PROGRESS = 'video.progress.set';
-    progress                      = 0;
-    duration                      = 0;
+    static readonly PING         = 'video.progress.ping';
+    static readonly PONG         = 'video.progress.pong';
+    static readonly GET_PROGRESS = 'video.progress.get';
+    static readonly SET_PROGRESS = 'video.progress.set';
+    progress                     = 0;
+    duration                     = 0;
 
     constructor(private player: any) {
         fromEvent(window, 'message')
-            .pipe(tap(event => console.log(event)))
+            .pipe(tap((event: any) => {
+                if (event.data.event) {
+                    console.log('PLUGIN', event);
+                }
+            }))
             .pipe(filter((event: any) => event.data.event === ProgressPlugin.GET_PROGRESS))
             .subscribe(event => {
                 this.progress = event.data.data;
@@ -54,7 +58,7 @@ export class ProgressPlugin {
 
     trackProgress() {
         window.postMessage({
-            event: ProgressPlugin.SEND_PROGRESS,
+            event: ProgressPlugin.SET_PROGRESS,
             data : (this.progress / this.duration) * 100
         }, '*');
     }
